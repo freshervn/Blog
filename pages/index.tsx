eb3ec4131eb3800e0ef5429f580f8d1b7abf4d9a
Your Name <dat.bt@agiletechsoftware.com>
2023-04-24 15:19:21 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,31 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <main>
-    <h1>Làm thế nào để thanh toán 2 dự án đang dở</h1>
-    <p>
-      
-    </p>
-  </main>;
+  return (
+    <main>
+      <h1>Làm thế nào để thanh toán 2 dự án đang dở</h1>
+      <h3>Nó đã bắt đầu như thế nào</h3>
+      <p>
+        Mình đã ước lượng một dự án làm hết một tháng và làm nó gần trong 1
+        tháng thật, tuy nhiên thời gian chờ api rồi gọi thêm người, rồi làm dự
+        án khác mà mãi mình chưa làm nó đó là bắt đầu việc bàn giao. và mình
+        không biết làm thế nào để kết thúc nó, giống như lúc trao đổi chiêu thức
+        trong game xong mình không kết liễu được đối thủ vậy.
+      </p>
+      <p>
+        Nếu giờ mình muốn kết thúc nó thì sao sự sợ hãi kiềm chế mình nghĩ xa
+        hơn những rủi ro dùng một kế hoặch khác đè lên để lẫn át nỗi sợ là cách
+        làm hợp lý lúc này.
+      </p>
+      <p>
+        Mình muốn nói với ai đó trước, ít nhất họ sẽ là backup của mình để mình
+        quay đầu nếu làm sai, một điểm tựa để nhấc bổng cả thế giới
+      </p>
+      <p>
+        Mình đang nghĩ đến anh thắng hoặc chị My, nếu mình định làm lâu lên chức gì đó thì mình cũng cần thân với anh chị chứ.
+        chỉ yếu là mình lại làm một đứa trẻ tiếp sao 
+        mọi người rất app lực về việc làm người trả 
+      </p>
+    </main>
+  );
 }
