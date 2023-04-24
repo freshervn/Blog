9b45f35c8af01dbe0550541dc5ee2c4d01597722
Your Name <dat.bt@agiletechsoftware.com>
2023-04-24 11:25:54 +0700

    init

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return <main>
+    <h1>Làm thế nào để thanh toán 2 dự án đang dở</h1>
+    <p>
+      
+    </p>
+  </main>;
+}
